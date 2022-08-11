55f442ae646ea5bfd9f71c39191b296c02ea6da5
Raphael Padua <raph.escpad@gmail.com>
2022-08-11 14:00:11 -0700

    Removed line

--- main.lua
+++ main.lua
@@ -5,17 +5,19 @@ function love.load()
     World = love.physics.newWorld(0,0)
     Map:box2d_init(World)
     Map.layers.solid.visible = false
-    
-
+    Player:load()
 end
 
 function love.update(dt)
     World:update(dt)
+    Player:update(dt)
 end
 
 function love.draw()
     Map:draw(0,0,2,2)
     love.graphics.push()
     love.graphics.scale(2,2)
+
+    Player:draw()
     love.graphics.pop()
 end
--- player.lua
+++ player.lua
@@ -5,12 +5,17 @@ function Player:load()
     self.y = 0
     self.width = 58 
     self.height = 78
+    self.xVel = 0
+    self.yVel = 0
+    self.maxSpeed = 200
+    self.acceleration = 4000
+
 
     self.physics = {}
     self.physics.body = love.physics.newBody(World,self.x,self.y,"dynamic")
     self.physics.body:setFixedRotation(true)
     self.physics.shape = love.physics.RectangleShape(self.width, self.height)
-    
+    self.physics.fixture = love.physics.newFixture(self.physics.body,self.physics.shape)
 end
 
 function Player:upate(dt)
@@ -18,5 +23,5 @@ function Player:upate(dt)
 end
 
 function Player:draw()
-
+    love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
 end
