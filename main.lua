6135adba9f5d420fe9d3787000b9849768e93aa9
Raphael Padua <raph.escpad@gmail.com>
2022-08-11 17:56:50 -0700

    Added movement controls

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 local STI = require("sti")
+require("player")
 
 function love.load()
     Map = STI("map/map.lua", {"box2d"})
     World = love.physics.newWorld(0,0)
+    World:setCallbacks(beginContact, endContact)
     Map:box2d_init(World)
     Map.layers.solid.visible = false
     Player:load()
@@ -20,4 +22,16 @@ function love.draw()
 
     Player:draw()
     love.graphics.pop()
+end
+
+function love.keypressed(key)
+    Player:jump(key)
+end
+
+function beginContact(a,b,collision)
+    Player:beginContact(a,b,collision)
+end
+
+function endContact(a,b,collision)
+    Player:endContact(a,b,collision)
 end
